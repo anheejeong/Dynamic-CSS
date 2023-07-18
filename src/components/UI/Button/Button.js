167259582cf5d984531b71ee03266bfce4be5f38
anheejeong <eyrt6973@naver.com>
2023-07-19 04:52:11 +0900

    learn dynamic css

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,42 @@
+import React from 'react';
+// css module : css 클래스나 css 파일을 가지고 그 클래스 이름을 기본적으로 고유하게 바꿈 
+import styles from './Button.module.css'; //using css module
+// import styled from 'styled-components'
+
+//import './Button.css';
+
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+
+// &:focus {
+//   outline: none;
+// }
+
+// &:hover,
+// &:active {
+//   background: #ac0e77;
+//   border-color: #ac0e77;
+//   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+// }
+// `; // taged template literal
+
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
